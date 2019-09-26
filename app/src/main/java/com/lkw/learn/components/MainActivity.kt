ac6ad5dd4f96bdd28ef70352aceb556eeacbafeb
liukewei <liukewei@yulintu.cn>
2019-09-26 11:26:28 +0800

    test

--- app/src/main/java/com/lkw/learn/components/MainActivity.kt
+++ app/src/main/java/com/lkw/learn/components/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.lkw.learn.components
 
 import android.os.Bundle
+import android.util.Log
 import com.lkw.learn.R
 import com.lkw.learn.base.BaseActivity
 
@@ -9,5 +10,6 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("lkw","")
     }
 }
