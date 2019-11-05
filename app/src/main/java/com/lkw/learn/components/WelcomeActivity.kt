ae02ffb6ba66711898bb6f8acf2492df035d896c
liukewei <liukewei@yulintu.cn>
2019-11-05 11:50:23 +0800

    assets 内置数据库

--- app/src/main/java/com/lkw/learn/components/WelcomeActivity.kt
+++ app/src/main/java/com/lkw/learn/components/WelcomeActivity.kt
@@ -4,15 +4,20 @@ import android.Manifest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.lkw.learn.R
 import com.lkw.learn.base.BaseActivity
+import com.lkw.learn.services.PathService
 import com.tbruyelle.rxpermissions2.RxPermissions
 import io.reactivex.functions.Consumer
+import java.io.*
+import java.nio.file.Path
 
 class WelcomeActivity : BaseActivity() {
     var permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
         requestPermission(permissions, Consumer { aBoolean ->
             if (!aBoolean) {
                 finish()
@@ -23,10 +28,38 @@ class WelcomeActivity : BaseActivity() {
     }
 
     private fun navTo() {
-        setContentView(R.layout.activity_welcome)
-        val i = Intent(this, MainActivity::class.java)
-        startActivity(i)
+        if (copySqlite()) {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }else{
+            Toast.makeText(this,"无法关联数据",Toast.LENGTH_LONG).show()
+        }
         finish()
+
+    }
+
+    /**
+     * 复制数据库文件
+     * return 是否继续
+     */
+    private fun copySqlite(): Boolean {
+
+        val dbFile = PathService.dbFile
+        if (dbFile.exists()) {
+            return true
+        }
+
+        val assetManager = this.resources.assets
+        val dbInputStream: InputStream?
+        val dbOutputStream: OutputStream = FileOutputStream(dbFile)
+        return try {
+            dbInputStream = assetManager.open(PathService.FILE_DB)
+            dbInputStream.copyTo(dbOutputStream)
+            true
+        } catch (e: IOException) {
+            e.printStackTrace()
+            false
+        }
     }
 
     private fun requestPermission(permissions: Array<String>, observer: Consumer<Boolean>) {
